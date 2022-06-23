78d1a3ccff4850509fa4201cbe5d65442a5a426d
Dani Rosa <danirosa.dev@gmail.com>
2022-06-22 22:57:00 -0300

    respostas da api

---
+++ script.js
@@ -0,0 +1,44 @@
+let mensagem;
+
+function entrarNaSala(){
+    let nome = prompt('Olá! Qual o seu nome?')
+    const nomeUsuario = {
+        nome: nome
+    }
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", nomeUsuario);
+    promise.then(buscarMsg);
+    promise.catch(alertaErro);
+}
+
+function buscarMsg(){
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    promise.then(pegaMsg);
+}
+
+function pegaMsg (resposta){
+    mensagem = resposta.data;
+    renderizaMsg();
+}
+
+function renderizaMsg(){
+    const ulMsg = document.querySelector('.mensagens');
+    ulMsg.innerHTML = "";
+
+    for (let i = 0; i < mensagem.length; i++) {
+        ulMsg.innerHTML += `
+        <li class="msg">
+                <span>(09:22:28) João para Todos: Bom dia</span>
+            </li>`
+    }
+}
+
+function enviarMsg(){}
+
+function alertaErro(error){
+    if (error.response.status === 404){
+        alert('Este nome já está sendo utilizado');
+        entrarNaSala();
+    }
+}
+
+entrarNaSala();
