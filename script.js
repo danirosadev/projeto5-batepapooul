af77f4bc1812a0894c83e23ce8162191d3d63304
Dani Rosa <danirosa.dev@gmail.com>
2022-06-25 18:17:01 -0300

    scroll automatico

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 let mensagem;
+let nome;
 
 function entrarNaSala(){
-    let nome = prompt('Olá! Qual o seu nome?')
+    nome = prompt('Olá! Qual o seu nome?');
     const nomeUsuario = {
-        nome: nome
-    }
+        name: nome
+    };
     const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", nomeUsuario);
-    promise.then(buscarMsg);
+    promise.then(buscaUsuario);
     promise.catch(alertaErro);
     buscarMsg();
 }
+
+function buscaUsuario(){
+    const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants');
+    promise.then(pegaUsuario);
+}
+
+function pegaUsuario(resposta){
+    nome = resposta.data;
+}
+
+function enviarMsg(){
+    let msgEscrita = document.querySelector('.aEnviar').value;
+    let novaMsg = {
+        from: nome,
+        to:'todos',
+        text: msgEscrita,
+        type:'message'
+    }
+
+    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', novaMsg);
+    promise.then(renderizaMsg);
+    alert('oi')
+}
+
 
 setInterval(buscarMsg, 3000);
 
@@ -23,6 +48,12 @@ function pegaMsg (resposta){
     renderizaMsg();
 }
 
+function scrollMsg(){
+    let mensagens = document.querySelectorAll('.mensagem');
+    let ultimaMsg = mensagens[mensagens.length - 1];
+    ultimaMsg.scrollIntoView();
+}
+
 function renderizaMsg(){
     const ulMsg = document.querySelector('.mensagens');
     
@@ -31,43 +62,33 @@ function renderizaMsg(){
     for (let i = 0; i < mensagem.length; i++) {
         if (mensagem[i].type === 'status'){
             ulMsg.innerHTML += `
-        <li class="entrada">
+        <li class="mensagem entrada">
             <span>${mensagem[i].time} ${mensagem[i].from} ${mensagem[i].text}...</span>
         </li>`
         } else if (mensagem[i].type === 'message'){
             ulMsg.innerHTML += `
-        <li class="msg">
+        <li class="mensagem msg">
             <span>${mensagem[i].time} ${mensagem[i].from} para ${mensagem[i].to}: ${mensagem[i].text}</span>
         </li>`
         }  else if (mensagem[i].type === 'private_message'){
             ulMsg.innerHTML += `
-        <li class="reservado">
+        <li class="mensagem reservado">
             <span>${mensagem[i].time} ${mensagem[i].from} reservadamente para ${mensagem[i].to}: ${mensagem[i].text}</span>
         </li>`
         }  
     }
+    scrollMsg();
         
 }
 
-function enviarMsg(){
-    let msgEscrita = document.querySelector('.aEnviar').value;
-    let novaMsg = {
-        from: 'dani',
-        to:'todos',
-        text: msgEscrita,
-        type:'message'
-    }
-
-    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', novaMsg);
-    promise.then(renderizaMsg);
-    alert('oi')
-}
 
 function alertaErro(error){
-    if (error.response.status === 404){
-        alert('Este nome já está sendo utilizado');
-        entrarNaSala();
-    }
+    // if (error.response.status === 400){
+    //     alert('Este nome já está sendo utilizado');
+    //     entrarNaSala();
+    // }
+
+    alert('deu erro')
 }
 
 entrarNaSala();
