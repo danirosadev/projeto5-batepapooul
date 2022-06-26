6ab71292e85bbfcba1467119ae3764bbe6b8f451
Dani Rosa <danirosa.dev@gmail.com>
2022-06-26 18:57:12 -0300

    requisitos ok

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let mensagem;
 let nome;
 let meuNome;
+let tipo;
+let destinatario;
 
 setInterval(buscarMsg, 3000);
+setInterval(buscaUsuario, 5000);
 
 function entrarNaSala(){
     meuNome = prompt('Olá! Qual o seu nome?');
@@ -28,8 +31,8 @@ function pegaUsuario(resposta){
 function enviarMsg(){
     
     let msgEscrita = document.querySelector('.aEnviar').value;
-    let tipo = 'message';
-    let destinatario = 'todos';
+    tipo = 'message';
+    destinatario = 'Todos';
 
     let novaMsg = {
         from: meuNome,
@@ -37,13 +40,18 @@ function enviarMsg(){
         text: msgEscrita,
         type: tipo
     }
-    console.log('novaMsg');
+
     const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', novaMsg);
     promise.then(buscarMsg);
-    msgEscrita.innerHTML = '';
-    alert('oi')
+    promise.catch(destAusente);
+    document.querySelector('.aEnviar').value = '';
 }
 
+function destAusente(){
+    alert('Destinatário ausente');
+    window.location.reload();
+
+}
 
 function buscarMsg(){
     const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
@@ -70,22 +78,25 @@ function renderizaMsg(){
         if (mensagem[i].type === 'status'){
             ulMsg.innerHTML += `
         <li class="mensagem entrada">
-            <span>${mensagem[i].time} ${mensagem[i].from} ${mensagem[i].text}...</span>
+            <span><span class="corCinza">(${mensagem[i].time})</span><span class="negrito">${mensagem[i].from}</span> ${mensagem[i].text}...</span>
         </li>`
         } else if (mensagem[i].type === 'message'){
             ulMsg.innerHTML += `
         <li class="mensagem msg">
-            <span>${mensagem[i].time} ${mensagem[i].from} para ${mensagem[i].to}: ${mensagem[i].text}</span>
+            <span><span class="corCinza">(${mensagem[i].time})</span><span class="negrito">${mensagem[i].from}</span> para <span class="negrito">${mensagem[i].to}</span>: ${mensagem[i].text}</span>
         </li>`
         }  else if (mensagem[i].type === 'private_message'){
-            ulMsg.innerHTML += `
-        <li class="mensagem reservado">
-            <span>${mensagem[i].time} ${mensagem[i].from} reservadamente para ${mensagem[i].to}: ${mensagem[i].text}</span>
-        </li>`
+            if (destinatario === meuNome){
+                ulMsg.innerHTML += `
+            <li class="mensagem reservado">
+                <span><span class="corCinza">(${mensagem[i].time})</span><span class="negrito">${mensagem[i].from}</span> reservadamente para <span class="negrito">${mensagem[i].to}</span>: ${mensagem[i].text}</span>
+            </li>`
+            }
         }  
     }
     scrollMsg();        
 }
+
 
 function alertaErro(error){
     if (error.response.status === 400){
